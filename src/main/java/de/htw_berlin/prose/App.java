6e7e0e333a14cb6a03cd631ab95f9a1d90fcd8ca
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2025-05-26 18:18:50 +0200

    Add usage examples for different cities

--- src/main/java/de/htw_berlin/prose/App.java
+++ src/main/java/de/htw_berlin/prose/App.java
@@ -1,6 +1,5 @@
 package de.htw_berlin.prose;
 
-import de.htw_berlin.fb4.ossd.prose.Prose;
 import de.htw_berlin.fb4.ossd.prose.ProseBuilder;
 
 import java.util.List;
@@ -8,26 +7,25 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
 
-        // === Teil 1: Eigene Klassen verwenden ===
-        SimpleSentence s1 = new SimpleSentence("Hello world!");
-        SimpleSentence s2 = new SimpleSentence("GitHub Packages are cool.");
+        SimpleWeatherSentence berlinWeather = new SimpleWeatherSentence();  // Berlin ist Standard
+        SimpleWeatherSentence bangkokWeather = new SimpleWeatherSentence("Bangkok");
+        SimpleWeatherSentence pragWeather = new SimpleWeatherSentence("Prag");
 
-        System.out.println("Wörter in Satz 1: " + s1.getWords());
-        System.out.println("Wörter in Satz 2: " + s2.getWords());
+        // with SimpleProse
+        SimpleProse simpleProse = new SimpleProse(List.of(berlinWeather, bangkokWeather, pragWeather
+        ));
 
-        SimpleProse simpleProse = new SimpleProse(List.of(s1, s2));
-
-        System.out.println("=== Ausgabe mit SimpleProse ===");
+        System.out.println("=== Output with SimpleProse ===");
         System.out.println(simpleProse.getText());
         System.out.println();
 
-        // === Teil 2: ProseBuilder verwenden ===
+        // with ProseBuilder
         ProseBuilder builder = new ProseBuilder();
-        builder.register(s1);
-        builder.register(s2);
+        builder.register(berlinWeather);
+        builder.register(bangkokWeather);
+        builder.register(pragWeather);
 
-        // Ausgabe mit builder.get()
-        System.out.println("=== Ausgabe mit ProseBuilder ===");
+        System.out.println("=== Output with ProseBuilder ===");
         System.out.println(builder.get());
     }
 }
